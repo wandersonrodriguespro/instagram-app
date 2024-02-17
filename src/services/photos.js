c05282a6d273f1f6a637d27ca406dc68f6c22c5b
Wanderson Rodrigues <wand_henrique@hotmail.com>
2024-02-17 16:12:53 -0300

    att api

--- src/services/photos.js
+++ src/services/photos.js
@@ -1,18 +1,20 @@
-import { createClient } from "pexels";
-
-const apiKey = import.meta.env.VITE_KEY_API_PEXELS;
-
-const client = createClient(apiKey);
-
 export const getPhotos = async (photosPerPage, callback) => {
+  const apiKey = import.meta.env.VITE_KEY_API_PEXELS;
+  const baseUrl = "https://api.pexels.com/v1/curated";
+
   let response = [];
 
   try {
-    response = await client.photos.curated({
-      page: 1,
-      per_page: photosPerPage,
-    });
-    return response?.photos;
+    response = await fetch(`${baseUrl}?page=1&per_page=${photosPerPage}`, {
+      method: "get",
+      headers: new Headers({
+        Authorization: apiKey,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => data.photos);
+
+      return response
   } catch (erro) {
     console.log(erro);
   } finally {
